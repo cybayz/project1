ec703cca95004d4f482511af9049a8e18a5062a7
cybayz <cybayz@gmail.com>
2020-10-24 22:10:09 +0530

    default route redirected to index

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,9 @@
 var express = require('express');
 const routes = require('express').Router();
 
+routes.get('/', (req, res) => {
+    res.render('index');
+})
 
 routes.get('/home', (req, res) => {
     res.render('index');
